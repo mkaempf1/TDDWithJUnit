f5dc1c5fb950f2c54291d1eccc7d980d4cf9f92f
Michael Kaempf <mkaempf1@student.cvtc.edu>
2015-04-08 01:29:18 +0000

    fix test3

--- test/CarTest.java
+++ test/CarTest.java
@@ -40,5 +40,10 @@ public class CarTest {
 	}
   
 	// Create and pass test for no deceleration below 0.
-	
+	@Test
+	public void cannotDecelerateBelowMinSpeed(){
+		Car car1 = new Car("Red", 100);
+		car1.decelerate(10);
+		assertThat(car1.getSpeed(), is(0));
+	}
 }
