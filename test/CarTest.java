d64d233b879b4fda3c37f2ace06529444c7f88f8
Michael Kaempf <mkaempf1@student.cvtc.edu>
2015-04-08 00:57:22 +0000

    redo test2

--- test/CarTest.java
+++ test/CarTest.java
@@ -32,11 +32,11 @@ public class CarTest {
 	}
 
 	// Create and pass test for deceleration.
-		@Test 
+	@Test 
 	public void canDecelerate(){
 		Car car1 = new Car("Red", 100);
 		car1.decelerate(10);
-		assertThat(car1.getSpeed(), is(10));
+		assertThat(car1.getSpeed(), is(0));
 	}
 	// Create and pass test for no deceleration below 0.
 
